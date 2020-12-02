c27393917f883910662cdf7bda8787a4c12682a7
Jitendra <jeetscmaker@gmail.com>
2020-12-03 01:01:53 +0530

    Transaction manager added for datasources.

--- multiple-datasource/src/main/java/org/example/config/Dbconfiguration.java
+++ multiple-datasource/src/main/java/org/example/config/Dbconfiguration.java
@@ -1,10 +1,13 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -21,5 +24,21 @@ public class Dbconfiguration {
     @ConfigurationProperties(prefix="spring.secondDatasource")
     public DataSource secondaryDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="tm1")
+    @Autowired
+    @Primary
+    DataSourceTransactionManager tm1(@Qualifier("datasource1") DataSource datasource) {
+        DataSourceTransactionManager txm  = new DataSourceTransactionManager(datasource);
+        return txm;
+    }
+    //@Transactional this will use the first datasource because it is @primary
+    // to use a specific t.m. use @Transactional("tm2") or @Transactional("tm1")
+    @Bean(name="tm2")
+    @Autowired
+    DataSourceTransactionManager tm2(@Qualifier ("datasource2") DataSource datasource) {
+        DataSourceTransactionManager txm  = new DataSourceTransactionManager(datasource);
+        return txm;
     }
 }
